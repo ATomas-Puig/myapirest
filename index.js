2542da526d290ebf340ec3a1738269e590c689c3
Antonio <atomas@elpuig.xeill.net>
2021-03-15 19:30:35 +0100

    M

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/mensajes', (req, res) => {
+    res.send("Caracolo");
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
